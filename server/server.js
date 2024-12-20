b37f01fc1dd695c10c33d2c1f0a93350e069db82
teletobi <58395640+teletobe@users.noreply.github.com>
2024-12-20 17:39:24 +0100

    try db fix

--- server/server.js
+++ server/server.js
@@ -5,12 +5,17 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 
 // MongoDB connection setup
 const mongoUri = process.env.MONGODB_URI;
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 const commentSchema = new mongoose.Schema({
   episode: { type: Number, required: true },
